cf298809c2acb6111a0fdc570d978c386ac0602a
jadhavsudharshan00-eng <jadhavsudharshan00@gmail.com>
2025-11-22 12:14:55 +0530

    Initialize Discord bot with message handling

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Bot is online as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("Pong! 🏓");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
